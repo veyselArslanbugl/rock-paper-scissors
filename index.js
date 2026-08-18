0b575d0c619cddcd901bf6f3e8dee136afb38965
Veysel <255936324+veyselArslanbugl@users.noreply.github.com>
2026-08-19 00:09:36 +0300

    Add playRound function and score variables

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//3.1
+let humanScore = 0;
+let computerScore = 0;
+
 //1
 
 function  getComputerChoice(){
@@ -19,3 +23,21 @@ function getHumanChoice(){
     return userInput.toLowerCase();
 }
 
+//3.2
+
+function playRound(humanChoice ,computerChoice){
+    if( humanChoice === computerChoice){
+        console.log("It's a tie! Both chose " + humanChoice); 
+    }else if(
+        (humanChoice === "rock" &&  computerChoice === "scissors")||
+        (humanChoice === "paper"  &&  computerChoice === "rock")  ||
+        (humanChoice === "scissors" && computerChoice === "paper")
+
+    ){
+        humanScore++;
+        console.log("You win! " + humanChoice + " beats " + computerChoice);
+    }else{
+        computerScore++;
+        console.log("You lose" + computerChoice + " beats " + humanChoice);
+    }
+}
